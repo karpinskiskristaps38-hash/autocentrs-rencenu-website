15c66b43330260f18a2029ae79ce7646ed501f46
karpinskiskristaps38-hash <karpinskiskristaps38@gmail.com>
2026-06-24 14:33:32 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
-import "@/App.css";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
-import { HOME } from "@/constants/testIds";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const API = `${BACKEND_URL}/api`;
@@ -15,7 +14,6 @@ const Home = () => {
     } catch (e) {
       console.error(e, `errored out requesting / api`);
     }
-  };
 
   useEffect(() => {
     helloWorldApi();
@@ -25,28 +23,26 @@ const Home = () => {
     <div>
       <header className="App-header">
         <a
-          data-testid={HOME.emergentLink}
+          data-testid="emergentLink"
           className="App-link"
           href="https://emergent.sh"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src="https://avatars.githubusercontent.com/in/1201222?s=120&u=2686cf91179bbafbc7a71bfbc43004cf9ae1acea&v=4" />
+          <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="Avatar" className="w-20 h-20 mx-auto" />
         </a>
         <p className="mt-5">Building something incredible ~!</p>
       </header>
     </div>
   );
-};
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}>
-            <Route index element={<Home />} />
-          </Route>
+          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
@@ -54,5 +50,3 @@ function App() {
 }
 
 export default App;
-
-
